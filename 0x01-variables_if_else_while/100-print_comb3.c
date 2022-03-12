81311b219351cbc1993557f9e8df652200e2a8fa
root <joetech.ooj@gmail.com>
2022-03-11 17:00:06 -0800

    Task 100

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main body of program
+ * Description: Print all single digit of base 10
+ * starting from 0, followed by a new line
+ * Return: 0
+ */
+int main(void)
+{
+	int i, j, k;
+
+	i = 0;
+
+	while (i < 100)
+	{
+		j = i % 10; /* singles digit */
+		k = i / 10; /* doubles digit */
+
+		if (k < j)
+		{
+			putchar(k + '0');
+			putchar(j + '0');
+
+			if (i < 89)
+			{
+				putchar(44);
+				putchar(32);
+			}
+		}
+
+		i++;
+	}
+	putchar('\n');
+
+	return (0);
+}
+
