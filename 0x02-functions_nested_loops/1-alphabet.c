864cf3f4fe63921e180f52cf5323757ecdcd5584
root <joetech.ooj@gmail.com>
2022-03-15 14:33:08 -0700

    Task 0

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+/**
+ * main - Entry point
+ *
+ * Description: prints _putchar using putchar prototype
+ *
+ * Return: Always 0 (Success)
+*/
+
+void print_alphabet(void)
+{
+	char ch;
+	ch = 'a';
+	while (ch <= 'z')
+	{
+		_putchar(ch);
+		ch++
+	}
+
+	_putchar("\n");
+}
