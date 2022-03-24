c2a1a8267d9ecb6b3d46c0cd066ae24ca17fbd82
root <joetech.ooj@gmail.com>
2022-03-24 14:16:27 -0700

    More Srings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenate two strings
+ * @dest: char string to concatenate to
+ * @src: char string
+ * Return: pointer to resulting string `dest`
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int x, y;
+
+	x = 0;
+
+	while (dest[x] != '\0')
+	{
+		i++;
+	}
+
+	y = 0;
+
+	while (src[y] != '\0')
+	{
+		dest[x] = src[y];
+		i++
+	}
+
+	dest[i] = '\0';
+	return (dest);
+	
+}
