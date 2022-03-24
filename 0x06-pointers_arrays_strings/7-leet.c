7245e56be7876cddf0d7ae84eac2de788b702c2b
root <joetech.ooj@gmail.com>
2022-03-24 16:52:30 -0700

    more strings

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * leet - encode a string into 1337
+ * @s: string
+ * Return: encoded string `s`
+ */
+
+char *leet(char *s)
+{
+	int x, y;
+
+	char s1[] = "aeotl";
+	char S1[] = "AEOTL";
+	char s2[] = "43071";
+
+	for (x = 0; s[x] != '\0'; x++)
+	{
+		for (y = 0; y < 5; y++)
+		{
+			if (s[x] == s1[y] || s[x] == S1[y])
+			{
+				s[x] = s2[y];
+				break;
+			}
+		}
+	}
+	return (s);
+}
