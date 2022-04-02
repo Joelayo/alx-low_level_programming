0d349fdba97c882ca837c4ce238432d21755076b
root <joetech.ooj@gmail.com>
2022-04-01 19:43:44 -0700

    Task 0

---
+++ 0x09-static_libraries/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen - Returns length of a string
+ * @s: String variable
+ * Return: no return
+ */
+
+int _strlen(char *s)
+{
+	int r;
+
+	r = 0;
+	while (s[r] != '\0')
+	{
+		r++;
+	}
+	return (r);
+}
