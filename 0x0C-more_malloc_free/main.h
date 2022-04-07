a8f74c5e9281038d34d0ecb52d85948fd6399712
root <joetech.ooj@gmail.com>
2022-04-07 14:03:27 -0700

    Task 1

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -9,7 +9,7 @@ void *malloc_checked(unsigned int b);
 
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 
-unsigned int _strlen(char *s)
+unsigned int _strlen(char *s);
 
 void *_calloc(unsigned int nmemb, unsigned int size);
 
