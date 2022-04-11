dac5336e5e1d175303bcc00dbc97f82f0c70a769
root <joetech.ooj@gmail.com>
2022-04-11 11:28:03 -0700

    Starting Strong

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * print_dog - information about dogs
+ * @d: pointer
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+		return;
+
+	if (d->name == NULL)
+		d->name = "(nil)";
+
+	if (d->owner == NULL)
+		d->owner = "(nil)";
+
+	printf("Name: %s\n", d->name);
+
+	printf("Age: %6f\n", d->age);
+
+	printf("Owner: %s\n", d->owner);
+
+}
