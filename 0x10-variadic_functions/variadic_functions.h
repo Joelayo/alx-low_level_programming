c17903096daf7176d6ac9b2a7d30d04a7da142a0
root <joetech.ooj@gmail.com>
2022-04-15 14:10:38 -0700

    main

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,12 +1,6 @@
 #ifndef _variadic_funtions_h_
 #define _variadic_funtions_h_
 
-typedef struct print_all
-{
-	char all;
-	void (*func)(va_list arguments);
-} simbol_t;
-
 int sum_them_all(const unsigned int n, ...);
 
 void print_numbers(const char *separator, const unsigned int n, ...);
